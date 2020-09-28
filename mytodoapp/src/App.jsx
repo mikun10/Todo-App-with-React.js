38fd24388e6543cb296cd5e9b06fceb4322efbd1
mikun <Mikun@gmail.com>
2020-09-28 23:40:30 +0530

    Initial Commit

---
+++ mytodoapp/src/App.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+ 
+
+const App = () => {
+
+  return(
+    <>
+
+<div className="main_div">
+  <div className="center_div">
+
+    <br/>
+    <h1> ToDo List</h1>
+    <br/>
+    <input type="text" placeholder="Add a Item"/>
+    <button>  + </button>
+    <ol>
+      <li>Buy Mango</li>
+    </ol>
+
+  </div>  
+
+</div>
+
+
+    </>
+  );
+};
+
+export default App;
