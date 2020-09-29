72fb02349dd45ca808981f60af2f884d0d573168
mikun <Mikun@gmail.com>
2020-09-30 00:27:14 +0530

    Updated App.jsx
    Added some eveents and used hooks.

--- mytodoapp/src/App.jsx
+++ mytodoapp/src/App.jsx
@@ -1,21 +1,43 @@
-import React from 'react';
+import React,{ useState } from "react";
  
 
 const App = () => {
 
+  const [inputList, setInputList] = useState(" ");
+const [Items ,setItems] =useState([]);
+
+
+
+  const itemEvent = (event)=>{
+    setInputList(event.target.value);
+
+  };
+
+   const listOfItems = ()=>{
+    setItems((oldItems) => {
+      return [...oldItems,inputList];
+    });
+   };
   return(
     <>
-
+  
 <div className="main_div">
   <div className="center_div">
 
     <br/>
     <h1> ToDo List</h1>
     <br/>
-    <input type="text" placeholder="Add a Item"/>
-    <button>  + </button>
+    <input type="text" placeholder="Add an Item" onChange={itemEvent} />
+    <button onClick={listOfItems}>  + </button>
+
     <ol>
-      <li>Buy Mango</li>
+    {/* <li>{inputList}</li> */}
+    {
+      Items.map((itemVal) =>{
+
+        return <li>{itemVal}</li>
+      })
+    }
     </ol>
 
   </div>  
